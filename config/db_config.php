7066887859efa2c53c856435befbd378f4ba2d0a
SVO2001 <sebastian.vallejo.ospina@hotmail.com>
2025-05-10 21:23:28 -0500

    cambio en db_config para establecer la conexión PDO sin SSL

--- config/db_config.php
+++ config/db_config.php
@@ -4,17 +4,12 @@ $db = 'proyecto';
 $user = 'adminusuario';
 $pass = 'Sebastian2001';
 
-// Ruta al archivo del certificado SSL
-$ssl_ca = __DIR__ . '/SSL/DigiCertGlobalRootCA.crt.pem';  
-
 try {
-    // Establecer la conexión PDO con el archivo SSL
+    // Establecer la conexión PDO sin SSL 
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass, array(
-        PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,  // Usar el archivo de certificado
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ));
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
 ?>
-
